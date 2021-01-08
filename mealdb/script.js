968f627f7b0dcf887461c65c9f08f6b75aad8ad4
opusidea <opusidea@gmail.com>
2021-01-08 09:52:47 +0000

    J5 am break

--- mealdb/script.js
+++ mealdb/script.js
@@ -1,7 +1,50 @@
-var API = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood";
+// Ciblage du DOM et globales
+var app                 = document.getElementById("app");
+var selectCategory      = document.getElementById("selectCategory");
+var gallery             = selectCategory.nextElementSibling;
 
-fetch(API)
+var API = "https://www.themealdb.com/api/json/v1/1/";
+
+
+function init() {
+    fetch(API + "filter.php?c=Seafood")
     .then(res => res.json())
     .then(res => {
-        console.log(res);
+        buildDom(res.meals);
     })
+
+    selectCategory.addEventListener("change", (e) => {
+        // attention: dans le cas d'une function fléchée "this" ,'est pas relié
+        // à l'élément du DOM ayant déclenché l'événement
+        // solution: utiliser le paramètre e, et e.target pour cibler cet élément
+        var selectedCategory = e.target.value;
+        if (selectedCategory != "") {
+            fetch(API + "filter.php?c=" + selectedCategory)
+                .then(res => res.json())
+                .then(res => {
+                    buildDom(res.meals);
+                })
+        } else { // pas de choix
+            gallery.innerHTML = "";
+        }
+    })
+}
+
+function buildDom(meals) {
+    gallery.innerHTML = ""; // reset
+    meals.forEach(meal => {
+        var img = document.createElement("img");
+        img.src = meal.strMealThumb;
+        img.alt = meal.strMeal;
+        img.id  = meal.idMeal;
+        img.classList.add("thumb");
+        img.addEventListener("click", mealDetails)
+        gallery.appendChild(img);
+    })
+}
+
+function mealDetails() {
+    console.log(this.id)
+}
+
+init();
